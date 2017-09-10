194d668fad056e613e6c89621ba1bc9e5c0c9713
yautomate1976 <yautomate@gmail.com>
2017-09-10 11:02:39 -0500

    recent updates to router.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + '/public'));
 
 var routes = require("./routes/routes.js")(app);
- 
-var server = app.listen(3000, function () {
+
+var server = app.listen(3000, function() {
     console.log("Listening on port %s...", server.address().port);
 });
